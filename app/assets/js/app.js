17921e452bb3626c32d15d671c42626804e33aaf
adriaanbalt <adriaan@balt.us>
2015-07-10 01:23:57 -0400

    app.js references details.js

--- app/assets/js/app.js
+++ app/assets/js/app.js
@@ -1,10 +1,11 @@
 (function () {
 	"use strict";
-	
+
 	require('angular');
 	require('angular-route');
 
 	var App = angular.module('balt', ['ngRoute']);
+    require('./details')(App);
 	require('./routes')(App);
 	require('./work')(App);
 
